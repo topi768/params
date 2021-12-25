6c75ad20afc9dc8fc6ba37bf90f63929913a9631
topi768 <90519641+topi768@users.noreply.github.com>
2021-12-25 21:53:16 +1000

    Updates

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// let screenWidth = window.screen.width
+//
+// function mobileStyle() {
+//   if (screenWidth >= 900) {
+//     console.log('sfe');
+//     var changingClassesInNavigation = document.getElementById("nav");
+//     changingClassesInNavigation.classList.toggle("burger-none");
+//   } else {
+//     var changingClasses = document.getElementById("screenWidth");
+//     changingClasses.classList.toggle("nav-none");
+//   }
+// }
+// mobileStyle();
+//
+//   function burger() {
+//     let body = document.getElementsByTagName( 'body' )[0];
+//     let html = document.getElementsByTagName( 'html' )[0]
+//     body.classList.toggle('lock');
+//     html.classList.toggle('lock');
+//   }
+
+///
+document.getElementById("progressBar1").value = 78;
+document.getElementById("progressBar2").value = 90;
+document.getElementById("progressBar3").value = 65;

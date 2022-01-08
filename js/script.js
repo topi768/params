9f783401f3dc55e964384d88ba6e2933abe5dfc0
topi768 <90519641+topi768@users.noreply.github.com>
2022-01-08 16:23:03 +1000

    Updates

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@
 function burger() {
   document.getElementById("menu-btn").classList.toggle('menu-btn_active');
   document.getElementById("body").classList.toggle('lock');
-
   event.preventDefault();
-
   document.getElementById("nav__content").classList.toggle('nav__content_active');
+}
+//
+
+let input = document.querySelectorAll('input');
+for(var i = 0; i < input.length; i++) {
+    input[i].addEventListener('click', function(){
+      let thisTask = this.parentNode.parentNode.parentNode.parentNode;
+      let thisAnswer = thisTask.querySelector('.answer');
+      thisAnswer.classList.toggle('answer--open')
+    }, false);
 }

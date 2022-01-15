ea7c148cb53433268b9e5aad26cb71f389202954
topi768 <90519641+topi768@users.noreply.github.com>
2022-01-16 01:01:41 +1000

    ready Dtags

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ let path={
     html: source_folder+"/**/*.html",
     css: source_folder + "/scss/**/*.scss",
     js: source_folder + "/js/**/*.js",
-    img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
+    img: source_folder + "/img/**/**/**/*.+(png|jpg|gif|ico|svg|webp)",
   },
    clean: "./" + project_folder + "/"
 }

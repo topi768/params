3ce356cc8544138bee9f4d82de19448b349e0ae7
topi768 <90519641+topi768@users.noreply.github.com>
2022-06-02 11:48:50 +1000

    Updates

--- js/script.js
+++ js/script.js
@@ -1,44 +1,59 @@
-// 3D Scroll
 
-let zSpacing = -1000,
-		lastPos = zSpacing / 5,
-		$frames = document.getElementsByClassName('frame'),
-		frames = Array.from($frames),
-		zVals = []
-
-window.onscroll = function() {
-
-	let top = document.documentElement.scrollTop,
-			delta = lastPos - top
-
-	lastPos = top
-
-	frames.forEach(function(n, i) {
-		zVals.push((i * zSpacing) + zSpacing)
-		zVals[i] += delta * -5.5
-		let frame = frames[i],
-				transform = `translateZ(${zVals[i]}px)`,
-				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
-		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
-	})
-
+document.body.onload = function () {
+  setTimeout(function() {
+    let preloaderOverlay = document.getElementById('preloaderOverlay');
+    if ( !preloaderOverlay.classList.contains('done') ) {
+      preloaderOverlay.classList.add('done');
+      document.getElementById("body").classList.remove('lock');
+    }
+  },300);
 }
+//
+function burger() {
+  document.getElementById("menu-btn").classList.toggle('menu-btn_active');
+  document.getElementById("body").classList.toggle('lock');
+  event.preventDefault();
+  document.getElementById("nav__content").classList.toggle('nav__content_active');
+}
+//
 
-window.scrollTo(0, 1)
-// Audio
+let input = document.querySelectorAll('input');
+for(let i = 0; i < input.length; i++) {
+    input[i].addEventListener('click', function(){
+      let thisTask = this.parentNode.parentNode.parentNode.parentNode;
+      let thisAnswer = thisTask.querySelector('.answer');
+      thisAnswer.classList.toggle('answer--open');
+      formulaDerivationSolution()
+    }, false);
+}
+// hover page
+// function hoverPage() {
+//   let title = document.getElementsByTagName("title")[0].innerHTML;
+//   let nav__items = document.querySelectorAll('.nav__item');
+//   for (let nav__item of nav__items) {
+//     if (nav__item.innerHTML == title) {
+//       nav__item.classList.add('nav__item__hover');
+//     }
+//   }
+// }
+ hoverPage()
+ //
+ function formulaDerivationSolution() {
+   containerWidth = document.querySelector(".task__container").offsetWidth - 0;
+   formulas = document.querySelectorAll("mjx-container");
 
-// let soundButton = document.querySelector('.soundbutton'),
-// 		audio = document.querySelector('.audio')
-//
-// soundButton.addEventListener('click', e => {
-// 	soundButton.classList.toggle('paused')
-// 	audio.paused ? audio.play() : audio.pause()
-// })
-//
-// window.onfocus = function() {
-// 	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
-// }
-//
-// window.onblur = function() {
-// 	audio.pause()
-// }
+   // function elementsWidth() {
+     for (let formula of formulas) {
+       var style = window.getComputedStyle(formula, null).getPropertyValue('font-size');
+       var fontSize = parseFloat(style);
+
+       if (formula.offsetWidth >  containerWidth) {
+         formula.style.fontSize =  "calc(10.8px + 3.2 * ((100vw - 320px)/ 1030) )" ;
+         // while (true) {
+         //   fontSize
+         // }
+       }
+     }
+   // }
+   // elementsWidth()
+ }
